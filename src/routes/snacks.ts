aecccba4062d2149f8d288f346c1a136e54a4ed4
Felipe Barbosa do Nascimento <dfelipebdn@gmail.com>
2023-07-25 12:03:59 -0300

    feat: Add checkSessionIdExists middleware to snacks routes

--- src/routes/snacks.ts
+++ src/routes/snacks.ts
@@ -2,6 +2,7 @@ import { randomUUID } from 'node:crypto'
 import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 import { knex } from '../database'
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
 
 export async function snacksRoutes(app: FastifyInstance) {
   app.post('/', async (req, res) => {
@@ -13,26 +14,61 @@ export async function snacksRoutes(app: FastifyInstance) {
 
     const { description, isOnDiet, name } = snacksSchema.parse(req.body)
 
+    let sessionId = req.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+      res.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
+      })
+    }
+
     await knex('snacks').insert({
       id: randomUUID(),
       name,
       is_on_diet: isOnDiet ? 1 : 0,
       description,
+      session_id: sessionId,
     })
 
     return res.status(201).send()
   })
 
-  app.get('/', async () => {
-    const snacks = knex('snacks').select()
-    return snacks
-  })
-  app.get('/:id', async (req) => {
-    const getSnackParamsSchema = z.object({
-      id: z.string().uuid(),
-    })
-    const { id } = getSnackParamsSchema.parse(req.params)
-    const snacks = knex('snacks').select('*').where('id', id)
-    return snacks
-  })
+  app.get(
+    '/',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (req) => {
+      const sessionId = req.cookies.sessionId
+      const snacks = await knex('snacks')
+        .select()
+        .where('session_id', sessionId)
+      return snacks
+    },
+  )
+  app.get(
+    '/:id',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (req, res) => {
+      const getSnackParamsSchema = z.object({
+        id: z.string().uuid(),
+      })
+      const { id } = getSnackParamsSchema.parse(req.params)
+
+      const sessionId = req.cookies.sessionId
+      const snack = await knex('snacks')
+        .select('*')
+        .where({
+          id,
+          session_id: sessionId,
+        })
+        .first()
+
+      return snack
+    },
+  )
 }
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify'
 import { env } from './env'
 import { snacksRoutes } from './routes/snacks'
+import cookie from '@fastify/cookie'
 
 const app = fastify()
 
+app.register(cookie)
 app.register(snacksRoutes, {
   prefix: 'snacks',
 })
